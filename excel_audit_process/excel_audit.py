905b6668b009626928b646de297690b737372334
deepakkumar-sm <smridee@outlook.com>
2025-12-21 16:32:31 +0530

    new files created

---
+++ excel_audit_process/excel_audit.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import openai
+from dotenv import load_dotenv
+
+# load excel files
+df_UBR = pd.read_excel("D:\First_Complex_Project\excel_audit_process\Excel_Audit_Project.xlsx", sheet_name="Sheet1")
+df_CVS = pd.read_excel("D:\First_Complex_Project\excel_audit_process\Excel_Audit_Project.xlsx", sheet_name="Sheet2")
+
+print(df_UBR)
+print() 
+print(df_CVS)
+
+
+# Initialize audit columns
+
+df_UBR["Check_Currency_UBR"] = ""
+df_UBR["Check_Amount_UBR"] = ""
+df_UBR["Check_Course_Term_UBR"] = ""
+df_UBR["Check_vendor_name_UBR"] = ""
+# df_UBR["Check_deliverydate_conflict_UBR"] = ""
+df_UBR["MI_Eligibility"] = ""
+
+
+# merge UBR with CVS on course name
+
+df_merged = df_UBR.merge(
+    df_CVS[["Course_Name","Currency","Amount","Course_Term","vendor_name"]],
+    on="Course_Name",
+    how="left",
+    suffixes=("","_CVS")
+)
+
+# helper function for comparison
+
+def compare_with_cvs(row, col, col_cvs):
+    if pd.isna(row[col_cvs]):
+        return "Course Not Found"
+    return "Match" if row[col] == row[col_cvs] else "Mismatch"
+
+# vectorized comparison
+df_merged["Check_Currency_UBR"] = df_merged.apply(lambda r: compare_with_cvs(r, "Currency", "Currency_CVS"), axis=1)
+df_merged["Check_Amount_UBR"] = df_merged.apply(lambda r: compare_with_cvs(r, "Amount", "Amount_CVS"), axis=1)
+df_merged["Check_Course_Term_UBR"] = df_merged.apply(lambda r: compare_with_cvs(r, "Course_Term", "Course_Term_CVS"), axis=1)
+df_merged["Check_vendor_name_UBR"] = df_merged.apply(lambda r: compare_with_cvs(r, "vendor_name", "vendor_name_CVS"), axis=1)
+
+df_merged.loc[df_merged["POD_Recd"] == "No", "MI_Eligibility"] = "Not Eligible"
+df_merged.loc[df_merged["POD_Recd"] == "Yes", "MI_Eligibility"] = "Eligible"
+
+
+
+# convert true/false to match/mismatch
+# df_merged["Check_Currency_UBR"] = df_merged["Check_Currency_UBR"].map({True: "Match", False: "Mismatch"})
+# df_merged["Check_Amount_UBR"] = df_merged["Check_Amount_UBR"].map({True: "Match", False: "Mismatch"})
+# df_merged["Check_Course_Term_UBR"] = df_merged["Check_Course_Term_UBR"].map({True: "Match", False: "Mismatch"})
+# df_merged["Check_vendor_name_UBR"] = df_merged["Check_vendor_name_UBR"].map({True: "Match", False: "Mismatch"})
+
+# save to new excel file
+df_merged.to_excel("D:\First_Complex_Project\excel_audit_process\MI_Audit_UBR_audited.xlsx", index=False)
+
+# Summary detailed report
